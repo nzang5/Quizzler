f6fde013ae95e2549fa8ea24b08490244e8e03dc
Nash Zangio <nash.zangio@qonto.com>
2024-11-15 15:14:45 +0100

    T or F logic added

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+
+    let quiz = [
+        ["Four + Two is equal to Six.", "True"],
+        ["Five - Three is greater than one.", "True"],
+        ["Three + Eight is less than ten.", "False"]
+    ]
+
+    var questionNumber = 0
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
 
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
 
+        let userAnswer = sender.currentTitle //True or False
+        let actualAnswer = quiz[questionNumber][1]
+
+        if userAnswer == actualAnswer{
+            print("Correct answer")
+        } else{
+            print("Wrong answer")
+        }
+        
+        questionNumber += 1
+        updateUI()
+    }
+
+    func updateUI(){
+        questionLabel.text = quiz[questionNumber][0]
+    }
 }
 
